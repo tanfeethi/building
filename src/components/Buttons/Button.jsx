499db313c0c5fe59f202067832cade3ca6fff621
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-23 16:58:32 +0200

    Make button component
    Make button component

---
+++ src/components/Buttons/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Button = ({ name }) => {
+    return (
+        <button 
+            style={{
+                width: "185px",
+                height: "56px",
+                border: "1px solid #1F69B3",
+                backgroundColor: "#1F69B3",
+                color: "white",
+                borderRadius: "5px",
+                fontSize: "18px",
+                fontWeight: "bold",
+                boxShadow: "0 4px 4px rgba(163, 163, 163, 0.4)",
+            }}
+        >
+            {name}
+        </button>
+    );
+}
+
+export default Button;
