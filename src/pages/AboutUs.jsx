3640db6c298cd275ee6eb59bf6b0e14028cd9a23
aymanhossam55 <asayed@tanfeethi.com.sa>
2025-01-25 23:45:16 +0200

    Update AboutUs.jsx
    Start Create About Us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero2 from '../components/Hero2/Hero2'
 
 const AboutUs = () => {
     return (
-        <div>AboutUs</div>
+        <div>
+            <Hero2 />
+        </div>
     )
 }
 
