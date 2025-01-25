b60fec4d3be2b960efab74bba6d921671b81c521
aymanhossam55 <asayed@tanfeethi.com.sa>
2025-01-25 23:45:51 +0200

    Update Contact.jsx
    Start Create Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero2 from '../components/Hero2/Hero2'
+import ContactUs from '../components/Contact/ContactUs'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div>
+            <Hero2 />
+            <ContactUs />
+        </div>
     )
 }
 
