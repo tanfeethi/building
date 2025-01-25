b1fdf21d82322e32fef87ea746da238fd9e4c716
aymanhossam55 <asayed@tanfeethi.com.sa>
2025-01-25 23:45:25 +0200

    Update AllProjects.jsx
    Start Create All Projects page

--- src/pages/AllProjects.jsx
+++ src/pages/AllProjects.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero2 from '../components/Hero2/Hero2'
 
 const AllProjects = () => {
     return (
-        <div>AllProjects</div>
+        <div>
+            <Hero2 />
+        </div>
     )
 }
 
