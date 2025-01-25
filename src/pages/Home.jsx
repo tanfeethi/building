bf916534b99da406e89a79db70514f3737dec748
aymanhossam55 <asayed@tanfeethi.com.sa>
2025-01-25 23:45:05 +0200

    Update Home.jsx
    Start Create Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Footer from '../components/Footer/Footer'
+import Expertise from '../components/Expertise/Expertise'
+import Sponsors from '../components/Sponsors/Sponsors'
+import Hero from '../components/Hero/Hero'
+import AboutUs from '../components/AboutUs/AboutUs'
 
 const Home = () => {
     return (
-        <div> <a href="/"></a>Home</div>
+        <div 
+        // className='mx-auto max-w-7xl'
+        >
+            {/* <Footer /> */}
+            <Hero />
+            <AboutUs />
+            <Sponsors />
+            <Expertise />
+            {/* Home */}
+        </div>
     )
 }
 
