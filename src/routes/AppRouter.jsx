ae9458809e7f5f5cbd3eb8a8702cebd11e83780e
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-26 15:56:41 +0200

    Update AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,7 +5,7 @@ import Home from '../pages/Home';
 import AboutUs from '../pages/AboutUs';
 import Services from '../pages/Services';
 import OurProjects from '../pages/OurProjects';
-import AllProjects from '../pages/AllProjects';
+// import AllProjects from '../pages/AllProjects';
 import ResidentialProject from '../pages/ResidentialProject';
 import Team from '../pages/Team';
 import Contact from '../pages/Contact';
@@ -21,8 +21,8 @@ const AppRouter = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<AboutUs />} />
                 <Route path="/services" element={<Services />} />
-                <Route path="/our-projects" element={<OurProjects />} />
-                <Route path="/all-projects" element={<AllProjects />} />
+                <Route path="/projects" element={<OurProjects />} />
+                {/* <Route path="/all-projects" element={<AllProjects />} /> */}
                 <Route path="/residential-project" element={<ResidentialProject />} />
                 <Route path="/team" element={<Team />} />
                 <Route path="/contact" element={<Contact />} />
