8437ff29d9d647c82f0707db220c4b4f9acbdf8a
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-26 15:58:01 +0200

    Update Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const Container = ({children}) => {
+const Container = ({ children }) => {
     return (
-        <div className='max-w-7xl mx-auto'>
+        <div className="mx-6xl">
             {children}
         </div>
-    )
-}
+    );
+};
 
-export default Container
+export default Container;
