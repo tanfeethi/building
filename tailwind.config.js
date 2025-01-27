debd507714739a2c43bc1ef80743598cac3790cd
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-27 16:12:02 +0200

    Update tailwind.config.js
    make media queries specialized for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,22 +42,22 @@ module.exports = {
         'large': '40px',
         'xlarge': '48px',
       },
-      // screens: {
-      //   'sm': {'min': '250px', 'max': '567px'},
-      //   // => @media (min-width: 640px and max-width: 767px) { ... }
+      screens: {
+        'sm': {'min': '250px', 'max': '567px'},
+        // => @media (min-width: 640px and max-width: 767px) { ... }
   
-      //   'md': {'min': '568px', 'max': '767px'},
-      //   // => @media (min-width: 768px and max-width: 1023px) { ... }
+        'md': {'min': '568px', 'max': '767px'},
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
   
-      //   'lg': {'min': '768px', 'max': '1280px'},
-      //   // => @media (min-width: 1024px and max-width: 1279px) { ... }
+        'lg': {'min': '768px', 'max': '1280px'},
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
   
-      //   'xl': {'min': '1281px', 'max': '1535px'},
-      //   // => @media (min-width: 1280px and max-width: 1535px) { ... }
+        'xl': {'min': '1281px', 'max': '1535px'},
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
   
-      //   // '2xl': {'min': '1536px'},
-      //   // // => @media (min-width: 1536px) { ... }
-      // },
+        '2xl': {'min': '1535px', 'max': '1920px'},
+        // // => @media (min-width: 1535px and max-width: 1920px) { ... }
+      },
   },
   }
 }
