6c6001f53bced62c7a2cac7f2f59d5451864fc02
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-27 16:13:04 +0200

    Update Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({ children }) => {
     return (
-        <div className="mx-6xl">
+        <div className="mx-6xl xl:mx-4xl lg:mx-2xl md:mx-xl sm:mx-m">
             {children}
         </div>
     );
