1dd17558a5a1164da1151f343752329f71f8dc9e
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-27 16:12:14 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,18 +5,19 @@ import AboutUs from '../components/AboutUs/AboutUs'
 import Services from '../components/Services/Services'
 import Testimonials from '../components/Testimoinals/Testimonials'
 import Timeline from '../components/Timeline/Timeline'
+import Experience from '../components/Experience/Experience'
 
 const Home = () => {
     return (
         <div 
         // className='mx-auto max-w-7xl'
         >
-            {/* <Footer /> */}
             <AboutUs />
+            <Experience />
             <Services />
             <Expertise />
             <Timeline />
-            {/* <Testimonials /> */}
+            <Testimonials />
             {/* Home */}
         </div>
     )
