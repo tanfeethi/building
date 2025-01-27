be1d0f851f3475832a9dec823c186e1acee90b35
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-27 16:12:56 +0200

    Update Projects2.jsx

--- src/components/Projects/Projects2.jsx
+++ src/components/Projects/Projects2.jsx
@@ -1,100 +1,127 @@
 import React from 'react';
 import Container from '../Container/Container';
-import servicesPhoto1 from '../../assets/servicesPhoto1.png'; // Replace with actual image
+import servicesPhoto1 from '../../assets/servicesPhoto1.png';
 
 const Projects2 = () => {
     return (
         <Container>
-            <h2 className="text-text-primary text-large font-bold mb-8 text-end">خدماتنا</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <h2 className="text-text-primary text-large font-bold mb-8 text-center">خدماتنا</h2>
+            <div className="grid grid-cols-1 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-2 gap-6 mb-8xl">
 
-                {/* Card 1 */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg group">
+                <div className="relative overflow-hidden shadow-lg group">
                     <img src={servicesPhoto1} alt="Service 1" className="w-full h-96 object-cover" />
-                    <div className="absolute right-0 bottom-0 w-full p-4 flex flex-col justify-end items-end bg-opacity-0 hover:bg-text-dark transition-all duration-300">
-                        <h4 className="group-hover:text-text-white m-2xl text-xl font-bold border-b-4 border-text-primary group-hover:border-b-4 group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            الأعمال الإلكترونية
+                    <div className="absolute right-0 bottom-0 w-full p-8 space-y-8 flex flex-col justify-end items-end bg-opacity-0 group-hover:bg-text-primary transition-all duration-300">
+                        <h4 className="text-xl font-bold border-b-4 border-text-primary text-text-primary group-hover:text-text-white group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            مشروع إنشاء فيلا سكنية
                         </h4>
+                        <div className="flex justify-between w-full text-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2">
+                            <p>تم التسليم</p>
+                            <p>سكني</p>
+                        </div>
                     </div>
                 </div>
 
-                {/* Card 2 */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg group">
-                    <img src={servicesPhoto1} alt="Service 2" className="w-full h-96 object-cover" />
-                    <div className="absolute right-0 bottom-0 w-full p-4 flex flex-col justify-end items-end bg-opacity-0 hover:bg-text-dark transition-all duration-300">
-                        <h4 className="group-hover:text-text-white m-2xl text-xl font-bold border-b-4 border-text-primary group-hover:border-b-4 group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            الإنشائيات
+                <div className="relative overflow-hidden shadow-lg group">
+                    <img src={servicesPhoto1} alt="Service 1" className="w-full h-96 object-cover" />
+                    <div className="absolute right-0 bottom-0 w-full p-8 space-y-8 flex flex-col justify-end items-end bg-opacity-0 group-hover:bg-text-primary transition-all duration-300">
+                        <h4 className="text-xl font-bold border-b-4 border-text-primary text-text-primary group-hover:text-text-white group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            مشروع إنشاء فيلا سكنية
                         </h4>
+                        <div className="flex justify-between w-full text-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2">
+                            <p>تم التسليم</p>
+                            <p>سكني</p>
+                        </div>
                     </div>
                 </div>
 
-                {/* Card 3 */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg group">
-                    <img src={servicesPhoto1} alt="Service 3" className="w-full h-96 object-cover" />
-                    <div className="absolute right-0 bottom-0 w-full p-4 flex flex-col justify-end items-end bg-opacity-0 hover:bg-text-dark transition-all duration-300">
-                        <h4 className="group-hover:text-text-white m-2xl text-xl font-bold border-b-4 border-text-primary group-hover:border-b-4 group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            الأعمال المعمارية
+                <div className="relative overflow-hidden shadow-lg group">
+                    <img src={servicesPhoto1} alt="Service 1" className="w-full h-96 object-cover" />
+                    <div className="absolute right-0 bottom-0 w-full p-8 space-y-8 flex flex-col justify-end items-end bg-opacity-0 group-hover:bg-text-primary transition-all duration-300">
+                        <h4 className="text-xl font-bold border-b-4 border-text-primary text-text-primary group-hover:text-text-white group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            مشروع إنشاء فيلا سكنية
                         </h4>
+                        <div className="flex justify-between w-full text-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2">
+                            <p>تم التسليم</p>
+                            <p>سكني</p>
+                        </div>
                     </div>
                 </div>
 
-                {/* Card 4 */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg group">
-                    <img src={servicesPhoto1} alt="Service 4" className="w-full h-96 object-cover" />
-                    <div className="absolute right-0 bottom-0 w-full p-4 flex flex-col justify-end items-end bg-opacity-0 hover:bg-text-dark transition-all duration-300">
-                        <h4 className="group-hover:text-text-white m-2xl text-xl font-bold border-b-4 border-text-primary group-hover:border-b-4 group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            الأعمال الكهربائية
+                <div className="relative overflow-hidden shadow-lg group">
+                    <img src={servicesPhoto1} alt="Service 1" className="w-full h-96 object-cover" />
+                    <div className="absolute right-0 bottom-0 w-full p-8 space-y-8 flex flex-col justify-end items-end bg-opacity-0 group-hover:bg-text-primary transition-all duration-300">
+                        <h4 className="text-xl font-bold border-b-4 border-text-primary text-text-primary group-hover:text-text-white group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            مشروع إنشاء فيلا سكنية
                         </h4>
+                        <div className="flex justify-between w-full text-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2">
+                            <p>تم التسليم</p>
+                            <p>سكني</p>
+                        </div>
                     </div>
                 </div>
 
-                {/* Card 5 */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg group">
-                    <img src={servicesPhoto1} alt="Service 5" className="w-full h-96 object-cover" />
-                    <div className="absolute right-0 bottom-0 w-full p-4 flex flex-col justify-end items-end bg-opacity-0 hover:bg-text-dark transition-all duration-300">
-                        <h4 className="group-hover:text-text-white m-2xl text-xl font-bold border-b-4 border-text-primary group-hover:border-b-4 group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            أعمال التكييف
+                <div className="relative overflow-hidden shadow-lg group">
+                    <img src={servicesPhoto1} alt="Service 1" className="w-full h-96 object-cover" />
+                    <div className="absolute right-0 bottom-0 w-full p-8 space-y-8 flex flex-col justify-end items-end bg-opacity-0 group-hover:bg-text-primary transition-all duration-300">
+                        <h4 className="text-xl font-bold border-b-4 border-text-primary text-text-primary group-hover:text-text-white group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            مشروع إنشاء فيلا سكنية
                         </h4>
+                        <div className="flex justify-between w-full text-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2">
+                            <p>تم التسليم</p>
+                            <p>سكني</p>
+                        </div>
                     </div>
                 </div>
 
-                {/* Card 6 */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg group">
-                    <img src={servicesPhoto1} alt="Service 6" className="w-full h-96 object-cover" />
-                    <div className="absolute right-0 bottom-0 w-full p-4 flex flex-col justify-end items-end bg-opacity-0 hover:bg-text-dark transition-all duration-300">
-                        <h4 className="group-hover:text-text-white m-2xl text-xl font-bold border-b-4 border-text-primary group-hover:border-b-4 group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            أعمال الصرف الصحي
+                <div className="relative overflow-hidden shadow-lg group">
+                    <img src={servicesPhoto1} alt="Service 1" className="w-full h-96 object-cover" />
+                    <div className="absolute right-0 bottom-0 w-full p-8 space-y-8 flex flex-col justify-end items-end bg-opacity-0 group-hover:bg-text-primary transition-all duration-300">
+                        <h4 className="text-xl font-bold border-b-4 border-text-primary text-text-primary group-hover:text-text-white group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            مشروع إنشاء فيلا سكنية
                         </h4>
+                        <div className="flex justify-between w-full text-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2">
+                            <p>تم التسليم</p>
+                            <p>سكني</p>
+                        </div>
                     </div>
                 </div>
 
-                {/* Card 7 */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg group">
-                    <img src={servicesPhoto1} alt="Service 7" className="w-full h-96 object-cover" />
-                    <div className="absolute right-0 bottom-0 w-full p-4 flex flex-col justify-end items-end bg-opacity-0 hover:bg-text-dark transition-all duration-300">
-                        <h4 className="group-hover:text-text-white m-2xl text-xl font-bold border-b-4 border-text-primary group-hover:border-b-4 group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            أعمال الزراعة
+                <div className="relative overflow-hidden shadow-lg group">
+                    <img src={servicesPhoto1} alt="Service 1" className="w-full h-96 object-cover" />
+                    <div className="absolute right-0 bottom-0 w-full p-8 space-y-8 flex flex-col justify-end items-end bg-opacity-0 group-hover:bg-text-primary transition-all duration-300">
+                        <h4 className="text-xl font-bold border-b-4 border-text-primary text-text-primary group-hover:text-text-white group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            مشروع إنشاء فيلا سكنية
                         </h4>
+                        <div className="flex justify-between w-full text-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2">
+                            <p>تم التسليم</p>
+                            <p>سكني</p>
+                        </div>
                     </div>
                 </div>
 
-                {/* Card 8 */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg group">
-                    <img src={servicesPhoto1} alt="Service 8" className="w-full h-96 object-cover" />
-                    <div className="absolute right-0 bottom-0 w-full p-4 flex flex-col justify-end items-end bg-opacity-0 hover:bg-text-dark transition-all duration-300">
-                        <h4 className="group-hover:text-text-white m-2xl text-xl font-bold border-b-4 border-text-primary group-hover:border-b-4 group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            أعمال المساحة
+                <div className="relative overflow-hidden shadow-lg group">
+                    <img src={servicesPhoto1} alt="Service 1" className="w-full h-96 object-cover" />
+                    <div className="absolute right-0 bottom-0 w-full p-8 space-y-8 flex flex-col justify-end items-end bg-opacity-0 group-hover:bg-text-primary transition-all duration-300">
+                        <h4 className="text-xl font-bold border-b-4 border-text-primary text-text-primary group-hover:text-text-white group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            مشروع إنشاء فيلا سكنية
                         </h4>
+                        <div className="flex justify-between w-full text-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2">
+                            <p>تم التسليم</p>
+                            <p>سكني</p>
+                        </div>
                     </div>
                 </div>
 
-                {/* Card 9 */}
-                <div className="relative rounded-lg overflow-hidden shadow-lg group">
-                    <img src={servicesPhoto1} alt="Service 9" className="w-full h-96 object-cover" />
-                    <div className="absolute right-0 bottom-0 w-full p-4 flex flex-col justify-end items-end bg-opacity-0 hover:bg-text-dark transition-all duration-300">
-                        <h4 className="group-hover:text-text-white m-2xl text-xl font-bold border-b-4 border-text-primary group-hover:border-b-4 group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                            الأعمال المعملية
+                <div className="relative overflow-hidden shadow-lg group">
+                    <img src={servicesPhoto1} alt="Service 1" className="w-full h-96 object-cover" />
+                    <div className="absolute right-0 bottom-0 w-full p-8 space-y-8 flex flex-col justify-end items-end bg-opacity-0 group-hover:bg-text-primary transition-all duration-300">
+                        <h4 className="text-xl font-bold border-b-4 border-text-primary text-text-primary group-hover:text-text-white group-hover:border-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            مشروع إنشاء فيلا سكنية
                         </h4>
+                        <div className="flex justify-between w-full text-text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 mt-2">
+                            <p>تم التسليم</p>
+                            <p>سكني</p>
+                        </div>
                     </div>
                 </div>
 
