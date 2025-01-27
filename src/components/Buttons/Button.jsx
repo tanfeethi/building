4db6ff9b41dc1a391dcd73bf505df54b376adabb
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-27 16:12:41 +0200

    Update Button.jsx

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-const Button = ({ name,className }) => {
+const Button = ({ name,className , style }) => {
     return (
         <button 
             className={className}
             style={{
-                width: "185px",
-                height: "56px",
-                // border: "1px solid #1F69B3",
-                // backgroundColor: "#1F69B3",
-                // color: "white",
-                borderRadius: "5px",
+                // width: "185px",
+                // height: "56px",
                 fontSize: "18px",
                 fontWeight: "bold",
                 boxShadow: "0 4px 4px rgba(163, 163, 163, 0.4)",
