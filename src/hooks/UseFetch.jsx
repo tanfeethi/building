0d390a7f61a4e801ef4560c4d7b788fbcf5414ae
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-28 15:52:49 +0200

    Update UseFetch.jsx

--- src/hooks/UseFetch.jsx
+++ src/hooks/UseFetch.jsx
@@ -1,8 +1,7 @@
-// useFetch.js
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { api_url } from "../utils/api";
 
-const useFetch = (url) => {
+const useFetch = (url, options = {}, lang = "ar") => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -10,17 +9,36 @@ const useFetch = (url) => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(url);
-                setData(response.data);
+                const response = await fetch(`${api_url}/${url}`, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "ngrok-skip-browser-warning": "true",
+                        Lang: lang,
+                        ...options.headers,
+                    },
+                    ...options,
+                });
+
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.statusText}`);
+                }
+
+                const result = await response.json();
+
+                if (result.data) {
+                    setData(result.data);
+                } else {
+                    throw new Error("No data found");
+                }
             } catch (err) {
-                setError(err);
+                setError(err.message || "Something went wrong");
             } finally {
                 setLoading(false);
             }
         };
 
         fetchData();
-    }, [url]);
+    }, []);
 
     return { data, loading, error };
 };
