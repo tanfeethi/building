d8f60bd074a1951d210ed39bb8afc4fa797f611b
Ayman428 <asayed@tanfeethi.com.sa>
2025-01-29 16:09:32 +0200

    Update Expertise.jsx

--- src/components/Expertise/Expertise.jsx
+++ src/components/Expertise/Expertise.jsx
@@ -9,20 +9,20 @@ const Expertise = () => {
                 <div className="flex flex-col 2xl:flex-row xl:flex-row lg:flex-row items-center justify-between mb-16 px-6 lg:px-0">
                     <div className="relative lg:w-1/2 w-full mb-8 lg:mb-0">
                         <div
-                            className="relative w-full h-[265px] bg-primary-dark overflow-hidden rounded-lg shadow-lg"
-                            style={{
-                                clipPath: "polygon(0 0, 60% 0%, 100% 100%, 0 100%)",
-                            }}
+                            className="relative w-full h-[265px] bg-primary-dark overflow-hidden rounded-lg shadow-lg transition-all duration-300 transform group"
                         >
                             <img
                                 src={ContainerPhoto}
+                                style={{
+                                    clipPath: "polygon(0 0, 60% 0%, 100% 100%, 0 100%)",
+                                }}
                                 alt="Expertise"
-                                className="absolute top-0 left-0 w-full h-full object-cover opacity-80"
+                                className="absolute top-0 left-0 w-full h-full object-cover opacity-80 group-hover:opacity-90 transition-all duration-300 group-hover:scale-110"
                             />
                             <div
                                 className="absolute bottom-0 left-0 right-32 p-4 text-white bg-text-primary"
                                 style={{
-                                    clipPath: "polygon(0 0, 85% 0%, 100% 100%, 0 100%)",
+                                    clipPath: "polygon(0 0, 90% 0%, 100% 100%, 0 100%)",
                                 }}
                             >
                                 <p className="text-sm">
@@ -40,9 +40,8 @@ const Expertise = () => {
                 </div>
 
                 <div className="container mx-auto grid grid-cols-1 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 gap-12 px-6 lg:px-0">
-
                     <div className="text-center group">
-                        <h3 className="text-lg font-bold text-black mb-4 border-b-2 border-text-primary-dark inline-block group-hover:text-text-primary group-hover:border-text-primary transition">
+                        <h3 className="text-lg font-bold text-misty mb-4 border-b-2 border-transparent inline-block text-text-primary group-hover:text-text-black group-hover:border-text-black transition-all duration-300">
                             خدمات البناء الشاملة
                         </h3>
                         <p className="text-medium text-text-black">
@@ -51,7 +50,7 @@ const Expertise = () => {
                     </div>
 
                     <div className="text-center group">
-                        <h3 className="text-lg font-bold text-black mb-4 border-b-2 border-text-primary-dark inline-block group-hover:text-text-primary group-hover:border-text-primary transition">
+                        <h3 className="text-lg font-bold text-misty mb-4 border-b-2 border-transparent inline-block text-text-primary group-hover:text-text-black group-hover:border-text-black transition-all duration-300">
                             العمل وفق ميزانية محددة
                         </h3>
                         <p className="text-medium text-text-black">
@@ -60,7 +59,7 @@ const Expertise = () => {
                     </div>
 
                     <div className="text-center group">
-                        <h3 className="text-lg font-bold text-black mb-4 border-b-2 border-text-primary-dark inline-block group-hover:text-text-primary group-hover:border-text-primary transition">
+                        <h3 className="text-lg font-bold text-misty mb-4 border-b-2 border-transparent inline-block text-text-primary group-hover:text-text-black group-hover:border-text-black transition-all duration-300">
                             متابعة مشروعك والإشراف عليه
                         </h3>
                         <p className="text-medium text-text-black">
